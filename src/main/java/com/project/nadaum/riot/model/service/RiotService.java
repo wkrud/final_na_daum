879cd54626650c7150dc36b0763dafddbb49699e
jackshin11 <jkshin9000@gmail.com>
2022-02-07 21:54:37 +0900

    커밋

--- src/main/java/com/project/nadaum/riot/model/service/RiotService.java
+++ src/main/java/com/project/nadaum/riot/model/service/RiotService.java
@@ -1,5 +1,8 @@
 package com.project.nadaum.riot.model.service;
 
+import java.util.Map;
+
+import com.project.nadaum.riot.model.vo.RiotFavo;
 import com.project.nadaum.riot.model.vo.Summoner;
 
 public interface RiotService {
@@ -7,5 +10,7 @@ public interface RiotService {
 	int insertSummoner(Summoner summoner);
 	
 	Summoner selectOneSummoner(String puuid);
+	
+	int insertRiotFavo(Map<String, Object> map);
 
 }
