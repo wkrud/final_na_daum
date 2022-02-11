35ce5fc7035f71c761a3c893f5f351b404247e0d
wkrud <99162203+wkrud@users.noreply.github.com>
2022-02-11 14:26:05 +0900

    Revert "Revert "Revert "Sp5na92"""

--- src/main/java/com/project/nadaum/culture/show/model/service/CultureService.java
+++ src/main/java/com/project/nadaum/culture/show/model/service/CultureService.java
@@ -3,20 +3,12 @@ package com.project.nadaum.culture.show.model.service;
 import java.util.List;
 import java.util.Map;
 
-import com.project.nadaum.culture.show.model.vo.Scrap;
-
+import com.project.nadaum.culture.show.model.vo.Culture;
 
 public interface CultureService {
 
 	int insertCultureLike(Map<String, Object> map);
 
 	int deleteCultureLike(String apiCode);
-
-	List<Scrap> selectCultureLikes(String id);
-
-	//===================================
-	int selectCountLikes(Map<String, Object> map);
-
-	int selectCountLikes(String apiCode);
 
 }
